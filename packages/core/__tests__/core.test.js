b5a4c18ca68b367f1afce45cf5d25927ddc262cf
AlfieriChou <alfierichou@gmail.com>
2020-11-24 21:10:34 +0800

    test: core test

--- packages/core/__tests__/core.test.js
+++ packages/core/__tests__/core.test.js
@@ -1,7 +1,21 @@
-'use strict';
+const path = require('path')
 
-const core = require('..');
+const loadModels = require('..')
 
-describe('core', () => {
-    it('needs tests');
-});
+describe('@galenjs core', () => {
+  it('load yaml', async done => {
+    const { remoteMethods, modelSchemas, schemas } = await loadModels(path.resolve(__dirname, './yaml'))
+    expect(typeof remoteMethods).toBe('object')
+    expect(typeof modelSchemas).toBe('object')
+    expect(typeof schemas).toBe('object')
+    done()
+  })
+
+  it('load json', async done => {
+    const { remoteMethods, modelSchemas, schemas } = await loadModels(path.resolve(__dirname, './json'))
+    expect(typeof remoteMethods).toBe('object')
+    expect(typeof modelSchemas).toBe('object')
+    expect(typeof schemas).toBe('object')
+    done()
+  })
+})

7adc4f5add8874ed8d091733247c3c771427d8c4
AlfieriChou <alfierichou@gmail.com>
2020-12-07 15:11:47 +0800

    feat: unique index

--- packages/sequelize-models/lib/createModel.js
+++ packages/sequelize-models/lib/createModel.js
@@ -2,8 +2,28 @@ const { jsonToModel } = require('./common')
 
 module.exports = ({
   model, modelName, tableName, plugins
-}, sequelize) => sequelize.define(modelName, jsonToModel(model), {
-  ...plugins,
-  underscored: true,
-  tableName
-})
+}, sequelize) => {
+  const options = {
+    ...plugins,
+    underscored: true,
+    tableName
+  }
+  // indexes
+  const indexes = Object.keys(model).reduce((acc, key) => {
+    // TODO: index
+    // if (model[key].index) {
+    //   return [...acc, {}]
+    // }
+    if (model[key].unique) {
+      return [...acc, {
+        unique: true,
+        fields: [key]
+      }]
+    }
+    return acc
+  }, [])
+  if (indexes.length) {
+    options.indexes = indexes
+  }
+  return sequelize.define(modelName, jsonToModel(model), options)
+}

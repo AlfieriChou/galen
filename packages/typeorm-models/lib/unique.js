2013a90107224c3fde7251f129c361ef2e4f9176
AlfieriChou <alfierichou@gmail.com>
2021-02-19 11:26:03 +0800

    feat: unique index

---
+++ packages/typeorm-models/lib/unique.js
@@ -0,0 +1,32 @@
+const _ = require('lodash')
+
+module.exports = ({ tableName, properties, indexes = {} }) => {
+  const keyUniques = Object.entries(properties).reduce((acc, [key, value]) => {
+    if (!value.unique) {
+      return acc
+    }
+    return [
+      ...acc,
+      {
+        name: `${tableName}_${_.snakeCase(key)}`,
+        columns: [_.snakeCase(key)]
+      }
+    ]
+  }, [])
+  const indexUniques = Object.entries(indexes).reduce((acc, [key, value]) => {
+    if (value.type !== 'unique') {
+      return acc
+    }
+    return [
+      ...acc,
+      {
+        name: key,
+        columns: value.fields
+      }
+    ]
+  }, [])
+  return [
+    ...keyUniques,
+    ...indexUniques
+  ]
+}

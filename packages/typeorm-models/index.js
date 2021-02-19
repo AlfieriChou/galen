7aa2887ab1151594e01dae0e8768638ac8694d15
AlfieriChou <alfierichou@gmail.com>
2021-02-19 16:11:59 +0800

    feat: create model

---
+++ packages/typeorm-models/index.js
@@ -0,0 +1,59 @@
+const { createConnection } = require('typeorm')
+const validateSchema = require('@galenjs/func/validateJsonSchema')
+
+const createEntity = require('./lib/entity')
+
+const createClient = async (entities, options) => createConnection({
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  entities,
+  synchronize: true,
+  logging: false,
+  ...options
+})
+
+module.exports = async (schemas, {
+  clients = {
+    main: {}
+  },
+  ...typeOrmOptions
+}) => {
+  await validateSchema(typeOrmOptions.default, {
+    type: 'object',
+    properties: {
+      host: { type: 'string' },
+      port: { type: 'number' },
+      database: { type: 'string' },
+      username: { type: 'string' },
+      password: { type: 'string' },
+      logging: { type: 'boolean' },
+      synchronize: { type: 'boolean' }
+    },
+    required: ['host', 'database', 'username', 'password']
+  })
+  const models = {}
+  const clientsEntities = Object.entries(schemas).reduce((acc, [key, value]) => {
+    if (value.dialect !== 'mysql') {
+      return acc
+    }
+    const clientName = value.databaseName || 'main'
+    const entities = acc[clientName] || []
+    models[key] = createEntity(value)
+    return {
+      [clientName]: [...entities, models[key]]
+    }
+  }, {})
+  await Object.keys(clients)
+    .reduce(async (promise, clientName) => {
+      const acc = await promise
+      return {
+        ...acc,
+        [clientName]: await createClient(clientsEntities[clientName] || [], {
+          ...typeOrmOptions.default,
+          ...clients[clientName]
+        })
+      }
+    }, Promise.resolve({}))
+  return models
+}

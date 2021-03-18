4ad3b0030db3970572dc5c821091afeeb10216ed
AlfieriChou <alfierichou@gmail.com>
2021-03-18 22:27:57 +0800

    feat: init framework

---
+++ packages/framework/__tests__/framework.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const framework = require('..');
+
+describe('framework', () => {
+    it('needs tests');
+});
---
+++ packages/framework/lib/framework.js
@@ -0,0 +1,6 @@
+
+module.exports = framework
+
+function framework () {
+  // TODO
+}

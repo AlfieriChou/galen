c6918128da7845b1fcf417539a45c4323fd49ea7
AlfieriChou <alfierichou@gmail.com>
2021-05-18 11:18:51 +0800

    feat: interval pull message

--- packages/amqp/index.js
+++ packages/amqp/index.js
@@ -13,19 +13,27 @@ module.exports = class Amqp {
       required: ['url', 'sub']
     })
     this.client = amqp.connect(this.config.url)
+    this.timers = {}
   }
 
   async quit () {
+    Object.entries(this.timers)
+      .forEach(([, interval]) => clearInterval(interval))
     await this.client.close()
   }
 
   async createChannels () {
     this.channel = await this.client.createChannel()
-    Promise.all(
-      Object.keys(this.config.sub)
-        .forEach(async ([key]) => {
+    await Promise.all(
+      Object.entities(this.config.sub)
+        .forEach(async ([key, {
+          pullInterval = 1000, // 默认1s拉一次消息
+          pullBatchSize = 5 // 默认每次拉五条
+        }]) => {
           await this.channel.assertQueue(key)
-        // create dynamic consume
+          this.timers[key] = setInterval(() => {
+            console.log('channel pull message', pullBatchSize)
+          }, pullInterval)
         })
     )
   }

a667af8cfd7213bb66f9a17119f2eb97ed01da83
AlfieriChou <alfierichou@gmail.com>
2021-06-04 16:01:37 +0800

    feat: load routes and listen

--- packages/framework/index.js
+++ packages/framework/index.js
@@ -1,19 +1,23 @@
 const Koa = require('koa')
-// const bodyParser = require('koa-bodyparser')
 
-const validateJsonSchema = require('@galenjs/func/validateJsonSchema')
+const validateJsonSchema = require('@galenjs/factories/validateJsonSchema')
 const loadModels = require('@galenjs/base')
+const buildRouter = require('@galenjs/koa-router')
+/* eslint-disable */
 const loadSequelizeModels = require('@galenjs/sequelize-models')
 const createRedisClients = require('@galenjs/redis')
+/* eslint-disable */
 
-const loadConfig = (config) => {
-  validateJsonSchema(config, {
+const gracefulExit = require('./lib/gracefulExit')
+
+const validateConfig = async (config) => {
+  await validateJsonSchema(config, {
     type: 'object',
     properties: {
       port: { type: 'number' },
       workspace: { type: 'string' },
       modelPath: { type: 'string' },
-      sequelizeOptions: {
+      sequelize: {
         type: 'object',
         properties: {
           default: {
@@ -40,7 +44,7 @@ const loadConfig = (config) => {
         },
         required: ['default', 'clients']
       },
-      redisOptions: {
+      redis: {
         type: 'object',
         properties: {
           default: {
@@ -67,24 +71,65 @@ const loadConfig = (config) => {
 
 module.exports = class Application {
   constructor (config) {
-    this.config = loadConfig(config)
+    this.config = config
+    this.pendingCount = 0
   }
 
   async init () {
+    await validateConfig(this.config)
     const app = new Koa()
     const { remoteMethods, modelSchemas, schemas } = await loadModels({
       workspace: this.config.workspace,
       modelPath: this.config.modelPath
     })
     app.context.remoteMethods = remoteMethods
+    this.remoteMethods = remoteMethods
     app.context.modelSchemas = modelSchemas
+    this.modelSchemas = modelSchemas
     app.context.schemas = schemas
-    if (this.config.sequelizeOptions) {
-      app.context.models = await loadSequelizeModels(modelSchemas, this.config.sequelizeOptions)
+    this.schemas = schemas
+    if (this.config.sequelize) {
+      app.context.models = await loadSequelizeModels(modelSchemas, this.config.sequelize)
     }
-    if (this.config.redisOptions) {
-      app.context.redis = await createRedisClients(this.config.redisOptions)
+    if (this.config.redis) {
+      app.context.redis = await createRedisClients(this.config.redis)
     }
-    return app
+    this.app = app
+    this.ctx = app.context
+  }
+
+  async loadRoutes () {
+    const router = await buildRouter({
+      remoteMethods: this.remoteMethods,
+      modelSchemas: this.modelSchemas
+    })
+    this.app.use(router.routes())
+    this.app.use(router.allowedMethods())
+  }
+
+  async closed () {
+    if (this.app.context.redis) {
+      await this.app.context.redis.quit()
+    }
+    if (this.app.context.models) {
+      await this.app.context.models.quit()
+    }
+  }
+
+  async listen () {
+    const server = this.app.listen(this.config.port, () => {
+      // eslint-disable-next-line no-console
+      console.log(`✅  The server is running at http://localhost:${this.config.port}`)
+    })
+  
+    gracefulExit(server, async () => {
+      if (this.pendingCount === 0) {
+        await this.closed()
+      } else {
+        this.app.on('pendingCount0', async () => {
+          await this.closed()
+        })
+      }
+    })
   }
 }
--- packages/framework/lib/framework.js
+++
@@ -1,6 +0,0 @@
-
-module.exports = framework
-
-function framework () {
-  // TODO
-}

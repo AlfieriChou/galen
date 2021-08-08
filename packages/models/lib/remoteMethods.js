f3e50ecf8a71219b202e35b29aaa914a901ffbb6
AlfieriChou <alfierichou@gmail.com>
2021-08-08 09:44:53 +0800

    feat: rewrite base to models

---
+++ packages/models/lib/remoteMethods.js
@@ -0,0 +1,122 @@
+const _ = require('lodash')
+
+const generateBaseRemoteMethods = (modelName, {
+  description = modelName,
+  properties,
+  required = []
+}) => ({
+  index: {
+    path: `/${modelName}`,
+    method: 'get',
+    tags: [`${modelName}`],
+    summary: `获取${description}列表`,
+    query: {
+      where: { type: 'json', description: '搜索条件 例如：where={}' },
+      order: { type: 'array', description: '排序 例如：order=[["createdAt","desc"]]' },
+      attribute: { type: 'array', description: '返回字段控制 例如：attribute=["id"]' },
+      include: { type: 'array', description: '关联表 关联查询 例如：include=[{"model":"UserRole"}]' },
+      offset: { type: 'integer', description: '分页偏移量 例如：offset=0' },
+      limit: { type: 'integer', description: '分页数量 例如：limit=20' }
+    },
+    output: {
+      200: {
+        type: 'object',
+        result: {
+          total: { type: 'integer', description: '总数' },
+          list: {
+            type: 'array',
+            items: {
+              type: 'object', properties
+            },
+            description: '数据'
+          }
+        }
+      }
+    }
+  },
+  create: {
+    path: `/${modelName}`,
+    method: 'post',
+    tags: [`${modelName}`],
+    summary: `创建${description}`,
+    requestBody: {
+      body: _.omit(properties, ['id', 'createdAt', 'updatedAt', 'deletedAt']),
+      required
+    },
+    output: {
+      200: {
+        type: 'object',
+        result: properties
+      }
+    }
+  },
+  show: {
+    path: `/${modelName}/:id`,
+    method: 'get',
+    tags: [`${modelName}`],
+    summary: `获取${description}详情`,
+    params: _.pick(properties, ['id']),
+    output: {
+      200: {
+        type: 'object',
+        result: properties
+      }
+    }
+  },
+  update: {
+    path: `/${modelName}/:id`,
+    method: 'put',
+    tags: [`${modelName}`],
+    summary: `修改${description}信息`,
+    params: _.pick(properties, ['id']),
+    requestBody: {
+      body: _.omit(properties, ['id', 'createdAt', 'updatedAt', 'deletedAt'])
+    },
+    output: {
+      200: {
+        type: 'array',
+        result: {
+          type: 'number'
+        }
+      }
+    }
+  },
+  destroy: {
+    path: `/${modelName}/:id`,
+    method: 'delete',
+    tags: [`${modelName}`],
+    summary: `删除${description}`,
+    params: _.pick(properties, ['id']),
+    output: {
+      200: {
+        type: 'number'
+      }
+    }
+  }
+})
+
+module.exports = (modelName, schema) => {
+  const { dialect, remoteMethods = {} } = schema
+  if (dialect && dialect === 'virtual') {
+    return remoteMethods
+  }
+  const baseRemoteMethods = generateBaseRemoteMethods(modelName, schema)
+  return {
+    ...baseRemoteMethods,
+    ...Object.entries(remoteMethods).reduce((acc, [key, value]) => {
+      if (baseRemoteMethods[key]) {
+        return {
+          ...acc,
+          [key]: {
+            ...baseRemoteMethods[key],
+            ...value
+          }
+        }
+      }
+      return {
+        ...acc,
+        [key]: value
+      }
+    }, {})
+  }
+}

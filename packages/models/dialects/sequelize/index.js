706fcd2f55f64b6ca6bb333f148257b06347d303
AlfieriChou <alfierichou@gmail.com>
2021-08-09 16:49:10 +0800

    feat: create model

--- packages/models/dialects/sequelize/index.js
+++ packages/models/dialects/sequelize/index.js
@@ -1,3 +1,20 @@
-const { Sequelize } = require('sequelize')
+const { Sequelize, Model } = require('sequelize')
+const createBaseModel = require('../../lib/baseModel')
+const parseModel = require('./lib/parseModel')
 
 exports.createDataSource = options => new Sequelize(options)
+
+exports.createModel = (dataSource, { modelDef, jsonSchema }, createModelFunc) => {
+  const BaseModel = createBaseModel(Model, dataSource, { modelDef, jsonSchema })
+  const model = createModelFunc ? createModelFunc(BaseModel) : BaseModel
+  model.init(
+    parseModel(modelDef.properties),
+    {
+      sequelize: dataSource,
+      modelName: modelDef.modelName,
+      underscored: true,
+      ...(modelDef.plugins || {})
+    }
+  )
+  return model
+}

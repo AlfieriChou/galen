ee75b174091e195709fd53dc0ff3ae05b3ecd53d
AlfieriChou <alfierichou@gmail.com>
2021-08-18 21:47:10 +0800

    fix: mixin model

--- packages/models/lib/mixinModel.js
+++ packages/models/lib/mixinModel.js
@@ -18,8 +18,9 @@ module.exports = async (dirPath, db) => {
     // eslint-disable-next-line import/no-dynamic-require, global-require
     const buildModel = require(filepath)
     const filename = path.basename(filepath).replace(/\.\w+$/, '')
-    const model = db[_.snakeCase(filename)] || class Model {}
-    models[_.camelCase(filename)] = buildModel(model)
+    const modelName = _.upperFirst(filename)
+    const model = db[modelName] || class Model {}
+    models[modelName] = buildModel(model)
   }))
 
   return models

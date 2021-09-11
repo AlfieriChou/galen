21549e76b6ebffe1ec0c1df93b19e8de22b91fc1
zhoudeyou <alfierichou@gmail.com>
2021-09-11 15:26:50 +0800

    style: eslint format

--- packages/factories/__tests__/customLimit.test.js
+++ packages/factories/__tests__/customLimit.test.js
@@ -1,37 +1,37 @@
 const CustomLimit = require('../customLimit')
 
-const customLimit = new CustomLimit();
+const customLimit = new CustomLimit()
 
 describe('CustomLimit', () => {
   it('execute', async () => {
-    const ret = await customLimit.execute(`${Date.now()}/1000`, () => 'helloWorld');
-    expect(ret).toEqual('helloWorld');
-    expect(customLimit.length()).toEqual(1);
-  });
+    const ret = await customLimit.execute(`${Date.now()}/1000`, () => 'helloWorld')
+    expect(ret).toEqual('helloWorld')
+    expect(customLimit.length()).toEqual(1)
+  })
 
   it('clear all', async () => {
-    await customLimit.execute(`${Date.now()}/1000`, () => 'helloWorld');
-    customLimit.clearAll();
-    expect(customLimit.length()).toEqual(0);
-  });
+    await customLimit.execute(`${Date.now()}/1000`, () => 'helloWorld')
+    customLimit.clearAll()
+    expect(customLimit.length()).toEqual(0)
+  })
 
   it('get value', async () => {
-    const key = `${Date.now()}/1000`;
-    await customLimit.execute(key, () => 'helloWorld');
-    const ret = customLimit.getValue(key);
-    expect(ret).toEqual('helloWorld');
-  });
+    const key = `${Date.now()}/1000`
+    await customLimit.execute(key, () => 'helloWorld')
+    const ret = customLimit.getValue(key)
+    expect(ret).toEqual('helloWorld')
+  })
 
   it('exists key', async () => {
-    const key = `${Date.now()}/100000`;
-    await customLimit.execute(key, () => 'helloWorld');
-    const ret = await customLimit.execute(key, () => 'kkWoeld');
-    expect(ret).toEqual('helloWorld');
-  });
+    const key = `${Date.now()}/100000`
+    await customLimit.execute(key, () => 'helloWorld')
+    const ret = await customLimit.execute(key, () => 'kkWoeld')
+    expect(ret).toEqual('helloWorld')
+  })
 
   it('cache length is 1', async () => {
-    await customLimit.execute(`${Date.now()}`, () => 'helloWorld');
-    await customLimit.execute(`${Date.now()}`, () => 'kkWoeld');
-    expect(customLimit.length()).toEqual(1);
-  });
-});
+    await customLimit.execute(`${Date.now()}`, () => 'helloWorld')
+    await customLimit.execute(`${Date.now()}`, () => 'kkWoeld')
+    expect(customLimit.length()).toEqual(1)
+  })
+})

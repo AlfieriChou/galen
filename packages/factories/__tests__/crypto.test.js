600402a53e74c7717d266665e500471cbdccd769
zhoudeyou <alfierichou@gmail.com>
2021-09-11 15:51:54 +0800

    feat: encrypted and decrypted

--- packages/factories/__tests__/crypto.test.js
+++ packages/factories/__tests__/crypto.test.js
@@ -1,8 +1,19 @@
-const { hash } = require('../crypto')
+const crypto = require('crypto')
+
+const { hash, encrypted, decrypted } = require('../crypto')
 
 describe('crypto', () => {
   it('hash', async () => {
     const ret = hash('test')
     expect(ret).toBe('098f6bcd4621d373cade4e832627b4f6')
+  })
+
+  it('aes crypto', () => {
+    const tt = 'abc'
+    const iv = crypto.randomBytes(16).toString('base64')
+    const key = crypto.randomBytes(16).toString('base64')
+    const encryptedData = encrypted(tt, { key, iv })
+    const decryptedData = decrypted(encryptedData, { key, iv })
+    expect(decryptedData).toEqual(tt)
   })
 })
--- packages/factories/crypto.js
+++ packages/factories/crypto.js
@@ -1,3 +1,31 @@
 const crypto = require('crypto')
 
 exports.hash = str => crypto.createHash('md5').update(str).digest('hex')
+
+exports.decrypted = (encryptedData, {
+  algorithm = 'aes-128-cbc', key, iv
+}) => {
+  const bufferData = Buffer.from(encryptedData, 'base64').toString('binary')
+  const decipher = crypto.createDecipheriv(
+    algorithm,
+    Buffer.from(key, 'base64'),
+    Buffer.from(iv, 'base64')
+  )
+  decipher.setAutoPadding(true)
+  let decoded = decipher.update(bufferData, 'binary', 'utf8')
+  decoded += decipher.final('utf8')
+  return decoded
+}
+
+exports.encrypted = (data, {
+  algorithm = 'aes-128-cbc', key, iv
+}) => {
+  const cipher = crypto.createCipheriv(
+    algorithm,
+    Buffer.from(key, 'base64'),
+    Buffer.from(iv, 'base64')
+  )
+  let ret = cipher.update(data, 'utf8', 'binary')
+  ret += cipher.final('binary')
+  return Buffer.from(ret, 'binary').toString('base64')
+}

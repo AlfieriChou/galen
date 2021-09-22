3fd2ca832b24061f79ea33ec462422c6b644ddc2
AlfieriChou <alfierichou@gmail.com>
2021-09-22 20:44:44 +0800

    refactor: bind to context

--- packages/framework-next/lib/context.js
+++ packages/framework-next/lib/context.js
@@ -13,93 +13,112 @@ const loadController = require('./loadController')
 const loadService = require('./loadService')
 const loadMiddleware = require('./loadMiddleware')
 
-module.exports = async config => {
-  const {
-    models,
-    modelDefs,
-    jsonSchemas,
-    remoteMethods
-  } = await createModels({
-    workspace: config.workspace,
-    modelDefPath: config.modelDefPath,
-    modelPath: config.modelPath,
-    config: config.models
-  })
+module.exports = async ({
+  workspace = process.pwd(),
+  modelDefPath = 'app/modelDef',
+  modelPath = 'app/models',
+  plugin = {},
+  ...config
+}) => {
+  if (config.models) {
+    const {
+      models,
+      modelDefs,
+      jsonSchemas,
+      remoteMethods
+    } = await createModels({
+      workspace,
+      modelDefPath,
+      modelPath,
+      config: config.models
+    })
+    Object.defineProperties(context, {
+      models: {
+        get () {
+          if (!this[CONTEXT_MODELS]) {
+            this[CONTEXT_MODELS] = models
+          }
+          return this[CONTEXT_MODELS]
+        }
+      },
+      modelDefs: {
+        get () {
+          if (!this[CONTEXT_MODEL_DEFS]) {
+            this[CONTEXT_MODEL_DEFS] = modelDefs
+          }
+          return this[CONTEXT_MODEL_DEFS]
+        }
+      },
+      jsonSchemas: {
+        get () {
+          if (!this[CONTEXT_JSON_SCHEMA]) {
+            this[CONTEXT_JSON_SCHEMA] = jsonSchemas
+          }
+          return this[CONTEXT_JSON_SCHEMA]
+        }
+      },
+      remoteMethods: {
+        get () {
+          if (!this[CONTEXT_REMOTE_METHOD]) {
+            this[CONTEXT_REMOTE_METHOD] = remoteMethods
+          }
+          return this[CONTEXT_REMOTE_METHOD]
+        }
+      }
+    })
+  }
 
-  const controller = await loadController({
-    workspace: config.workspace,
-    controllerPath: config.controllerPath,
-    plugin: config.plugin || {}
-  })
+  if (config.controllerPath) {
+    const controller = await loadController({
+      workspace,
+      controllerPath: config.controllerPath,
+      plugin
+    })
+    Object.defineProperties(context, {
+      controller: {
+        get () {
+          if (!this[CONTEXT_CONTROLLER]) {
+            this[CONTEXT_CONTROLLER] = controller
+          }
+          return this[CONTEXT_CONTROLLER]
+        }
+      }
+    })
+  }
 
-  const service = await loadService({
-    workspace: config.workspace,
-    servicePath: config.servicePath,
-    plugin: config.plugin || {}
-  })
+  if (config.servicePath) {
+    const service = await loadService({
+      workspace,
+      servicePath: config.servicePath,
+      plugin
+    })
+    Object.defineProperties(context, {
+      service: {
+        get () {
+          if (!this[CONTEXT_SERVICE]) {
+            this[CONTEXT_SERVICE] = service
+          }
+          return this[CONTEXT_SERVICE]
+        }
+      }
+    })
+  }
 
-  const middleware = await loadMiddleware({
-    workspace: config.workspace,
-    middlewarePath: config.middlewarePath,
-    plugin: config.plugin || {}
-  })
-
-  Object.defineProperties(context, {
-    models: {
-      get () {
-        if (!this[CONTEXT_MODELS]) {
-          this[CONTEXT_MODELS] = models
+  if (config.middlewarePath) {
+    const middleware = await loadMiddleware({
+      workspace,
+      middlewarePath: config.middlewarePath,
+      plugin
+    })
+    Object.defineProperties(context, {
+      middleware: {
+        get () {
+          if (!this[CONTEXT_MIDDLEWARE]) {
+            this[CONTEXT_MIDDLEWARE] = middleware
+          }
+          return this[CONTEXT_MIDDLEWARE]
         }
-        return this[CONTEXT_MODELS]
       }
-    },
-    modelDefs: {
-      get () {
-        if (!this[CONTEXT_MODEL_DEFS]) {
-          this[CONTEXT_MODEL_DEFS] = modelDefs
-        }
-        return this[CONTEXT_MODEL_DEFS]
-      }
-    },
-    jsonSchemas: {
-      get () {
-        if (!this[CONTEXT_JSON_SCHEMA]) {
-          this[CONTEXT_JSON_SCHEMA] = jsonSchemas
-        }
-        return this[CONTEXT_JSON_SCHEMA]
-      }
-    },
-    remoteMethods: {
-      get () {
-        if (!this[CONTEXT_REMOTE_METHOD]) {
-          this[CONTEXT_REMOTE_METHOD] = remoteMethods
-        }
-        return this[CONTEXT_REMOTE_METHOD]
-      }
-    },
-    controller: {
-      get () {
-        if (!this[CONTEXT_CONTROLLER]) {
-          this[CONTEXT_CONTROLLER] = controller
-        }
-        return this[CONTEXT_CONTROLLER]
-      }
-    },
-    service: {
-      get () {
-        if (!this[CONTEXT_SERVICE]) {
-          this[CONTEXT_SERVICE] = service
-        }
-        return this[CONTEXT_SERVICE]
-      }
-    },
-    middleware: {
-      get () {
-        if (!this[CONTEXT_MIDDLEWARE]) {
-          this[CONTEXT_MIDDLEWARE] = middleware
-        }
-        return this[CONTEXT_MIDDLEWARE]
-      }
-    }
-  })
+    })
+  }
 }

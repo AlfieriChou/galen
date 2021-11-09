f8cacf595e2adf26ccdf20ebebaf84e491269f9d
zhoudeyou <alfierichou@gmail.com>
2021-11-09 15:46:14 +0800

    fix: rollback load service

--- packages/framework-next/lib/loadService.js
+++ packages/framework-next/lib/loadService.js
@@ -1,19 +1,21 @@
 const classLoader = require('@galenjs/class-loader')
 const fs = require('fs')
 const path = require('path')
-const _ = require('lodash')
 
 module.exports = async ({
   workspace, servicePath, plugin
 }) => {
-  const service = {}
+  let service = {}
   if (servicePath) {
     const serviceDirPath = path.join(
       workspace,
       `/${servicePath}`
     )
     if (fs.existsSync(serviceDirPath)) {
-      _.merge(service, classLoader(serviceDirPath))
+      service = {
+        ...service,
+        ...classLoader(serviceDirPath)
+      }
     }
     if (plugin) {
       await Promise.all(plugin.plugins.map(async pluginName => {
@@ -23,7 +25,10 @@ module.exports = async ({
           `/${pluginMainPath}/${pluginName}/${servicePath}`
         )
         if (fs.existsSync(pluginServiceDirPath)) {
-          _.merge(service, classLoader(pluginServiceDirPath))
+          service = {
+            ...service,
+            ...classLoader(pluginServiceDirPath)
+          }
         }
       }))
     }

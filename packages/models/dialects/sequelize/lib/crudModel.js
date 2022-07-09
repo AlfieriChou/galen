cff2663c5929eb135169a0ea91832b038e616212
AlfieriChou <alfierichou@gmail.com>
2022-07-09 22:03:57 +0800

    fix: sequelize model name is null

--- packages/models/dialects/sequelize/lib/crudModel.js
+++ packages/models/dialects/sequelize/lib/crudModel.js
@@ -3,7 +3,7 @@ const sequelizeQueryFilter = require('@galenjs/sequelize-query-filter')
 module.exports = Model => {
   return class extends Model {
     toJSON () {
-      const values = { ...this.get({ plain: true }) }
+      const values = { ...Model.get({ plain: true }) }
       if (typeof values === 'object') {
         if (Array.isArray(values)) {
           return values.map(value => this.$formatJson(value))
@@ -28,47 +28,44 @@ module.exports = Model => {
     static async remoteFindPage (ctx) {
       const { request: { query } } = ctx
       const filter = sequelizeQueryFilter(query, ctx.models)
-      const [total, list] = await Promise.all([
-        await this.count(filter),
-        await this.findAll(filter)
-      ])
+      const { count, rows } = await Model.findAndCountAll(filter)
       return {
-        list: list.map(item => item.toJSON()),
+        list: rows.map(item => item.toJSON()),
         pageInfo: {
-          totalNumber: total,
+          totalNumber: count,
           page: (filter.offset + filter.limit) / filter.limit,
           pageSize: filter.limit,
-          totalPage: Math.ceil(total / filter.limit)
+          totalPage: Math.ceil(count / filter.limit)
         }
       }
     }
 
     static async remoteCreate (ctx) {
       const { request: { body } } = ctx
-      const inst = await this.create(body)
+      const inst = await Model.create(body)
       return inst.toJSON()
     }
 
     static async remoteBatchCreate (ctx) {
       const { request: { body } } = ctx
-      const ret = await this.bulkCreate(body.data)
+      const ret = await Model.bulkCreate(body.data)
       return ret.map(item => item.toJSON())
     }
 
     static async remoteShow (ctx) {
       const { params: { id } } = ctx
-      const inst = await this.findByPk(id)
+      const inst = await Model.findByPk(id)
       return inst.toJSON()
     }
 
     static async remoteUpdate (ctx) {
       const { request: { body }, params: { id } } = ctx
-      return this.update(body, { where: { id } })
+      return Model.update(body, { where: { id } })
     }
 
     static async remoteDestroy (ctx) {
       const { params: { id } } = ctx
-      return this.destroy({ where: { id } })
+      return Model.destroy({ where: { id } })
     }
   }
 }

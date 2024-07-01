8a28a0582bf59d41c4b19dc50672d6312bf85f88
AlfieriChou <alfierichou@gmail.com>
2024-07-01 22:13:42 +0800

    feat: hmac crypto

--- packages/factories/crypto.js
+++ packages/factories/crypto.js
@@ -1,4 +1,4 @@
-const crypto = require('crypto')
+const crypto = require('node:crypto')
 
 exports.hash = str => crypto.createHash('md5').update(str).digest('hex')
 
@@ -28,4 +28,37 @@ exports.encrypted = (data, {
   let ret = cipher.update(data, 'utf8', 'binary')
   ret += cipher.final('binary')
   return Buffer.from(ret, 'binary').toString('base64')
+}
+
+const sortObject = o => {
+  if (Array.isArray(o)) { return o.sort() }
+  if (!o || typeof o !== 'object') {
+    return o
+  }
+  const keys = Object.keys(o)
+  return keys.sort().map(
+    key => [key, sortObject(o[key])]
+  )
+}
+
+exports.sortObject = sortObject
+
+exports.base64 = (str, encoding = 'utf8') => {
+  return Buffer.from(str, encoding).toString('base64')
+}
+
+const updateData = (hash, data, encoding = 'utf8') => {
+  const isBuffer = Buffer.isBuffer(data)
+  let toUpdateData = data
+  if (!isBuffer && typeof data === 'object') {
+    toUpdateData = JSON.stringify(sortObject(data))
+  }
+  return hash.update(toUpdateData, isBuffer ? 'binary' : encoding)
+}
+
+exports.hmac = (key, data, algorithm = 'sha256', format = 'hex') => {
+  return updateData(
+    crypto.createHmac(algorithm, key),
+    data
+  ).digest(format)
 }
